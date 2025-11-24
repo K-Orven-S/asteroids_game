f8aa46f722d3a0ad22b8ae7d97aea749c1e53efb
K-Orven-S <kimorvens@gmail.com>
2025-11-25 05:42:45 +0800

    Initial commit for asteroids game

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+
+def main():
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
