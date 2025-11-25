b0db2ea404a4e5a0046794390fb5555dad9c2673
K-Orven-S <kimorvens@gmail.com>
2025-11-25 15:08:44 +0800

    add rotate and move in player class

---
+++ asteroid.py
@@ -0,0 +1,4 @@
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+import pygame
+
--- main.py
+++ main.py
@@ -11,7 +11,13 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
 
     while True:
         log_state()
@@ -21,7 +27,13 @@ def main():
                 return
 
         screen.fill("black")
-        player.draw(screen)
+        
+        for unit in drawable:
+            unit.draw(screen)
+
+        updatable.update(dt)
+
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
