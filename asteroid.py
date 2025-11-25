36458e79dbf792db7351b527ca9ca3c488efbb04
K-Orven-S <kimorvens@gmail.com>
2025-11-25 16:08:09 +0800

    add asteroids

--- asteroid.py
+++ asteroid.py
@@ -2,3 +2,12 @@ from circleshape import CircleShape
 from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, PLAYER_RADIUS)
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", center=(self.position.x, self.position.y), radius=PLAYER_RADIUS, width=LINE_WIDTH)
+    
+    def update(self, dt):
+        self.position += (self.velocity * dt)
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import *
 from logger import log_state
 from player import Player
-
-
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 
 
 def main():
@@ -14,10 +14,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    
+    asteroid_field = AsteroidField()
 
     while True:
         log_state()
@@ -31,15 +35,13 @@ def main():
         for unit in drawable:
             unit.draw(screen)
 
-        updatable.update(dt)
+        for unit2 in updatable:
+            unit2.update(dt)
 
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
-
-        
-
 
 if __name__ == "__main__":
     main()
